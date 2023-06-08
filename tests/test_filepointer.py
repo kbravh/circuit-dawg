e332108e016acf318da97756a0294cb6afde9944
Karey Higuera <karey.higuera@gmail.com>
2023-06-07 22:12:59 -0400

    add seek and skip tests to file pointer

--- tests/test_filepointer.py
+++ tests/test_filepointer.py
@@ -3,39 +3,47 @@ import io
 from circuit_dawg.wrapper import FilePointer
 
 
-def test_seek_and_read():
-    test_io = io.BytesIO(b"\x00\x00\x00\x00Hello World!")
-    fp = FilePointer(test_io)
+class TestFilePointer:
+    @pytest.fixture(autouse=True, scope="function", name="fp")
+    def setup(self):
+        test_io = io.BytesIO(b"\x00\x01\x00\x00Hello World!")
+        return FilePointer(test_io)
 
-    # The first seek should skip the first 4 bytes
-    fp.seek(0)
-    assert fp.read(5) == b"Hello"
+    def test_seek_and_read(self, fp):
+        # The first seek should skip the first 4 bytes
+        fp.seek(0)
+        assert fp.read(5) == b"Hello"
 
-    # Subsequent seeks should be offset by 4
-    fp.seek(6)
-    assert fp.read(5) == b"World"
+        # Subsequent seeks should be offset by 4
+        fp.seek(6)
+        assert fp.read(5) == b"World"
 
+    def test_close(self, fp):
+        # Close should work without raising an exception
+        try:
+            fp.close()
+        except Exception:
+            pytest.fail("Unexpected Exception raised")
 
-def test_close():
-    test_io = io.BytesIO(b"\x00\x00\x00\x00Hello World!")
-    fp = FilePointer(test_io)
+    def test_destructor(self, fp):
+        # Delete should work without raising an exception
+        try:
+            del fp
+        except Exception:
+            pytest.fail("Unexpected Exception raised")
 
-    # Close should work without raising an exception
-    try:
-        fp.close()
-    except Exception:
-        pytest.fail("Unexpected Exception raised")
+    def test_base_size(self, fp):
+        """
+        The base_size should be the first 4 bytes of the file.
+        The FilePointer should then adjust the skip to be 4 bytes.
+        """
+        assert fp.base_size == 256  # 0x0100
+        assert fp.skip == 4
 
-
-def test_destructor():
-    test_io = io.BytesIO(b"\x00\x00\x00\x00Hello World!")
-    fp = FilePointer(test_io)
-
-    # Delete should work without raising an exception
-    try:
-        del fp
-    except Exception:
-        pytest.fail("Unexpected Exception raised")
-
-
-# Add more tests as necessary
+    @pytest.mark.parametrize("skip", [(0, 4), (4, 8), (8, 12)])
+    def test_seek(self, fp, skip):
+        """
+        The seek should be adjusted by the skip.
+        """
+        fp.seek(skip[0])
+        assert fp.fp.tell() == skip[1]
